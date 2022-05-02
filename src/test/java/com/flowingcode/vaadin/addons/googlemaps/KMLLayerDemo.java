e2de5d9655a73769a1cabd1c583666ac49b93cb9
Paola De Bartolo <paola.debartolo@gmail.com>
2022-05-02 17:38:35 -0300

    feat(demo): add kml demo

---
+++ src/test/java/com/flowingcode/vaadin/addons/googlemaps/KMLLayerDemo.java
@@ -0,0 +1,40 @@
+/*-
+ * #%L
+ * Google Maps Addon
+ * %%
+ * Copyright (C) 2020 - 2021 Flowing Code
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package com.flowingcode.vaadin.addons.googlemaps;
+
+import com.flowingcode.vaadin.addons.demo.DemoSource;
+import com.flowingcode.vaadin.addons.googlemaps.GoogleMap.MapType;
+import com.vaadin.flow.router.PageTitle;
+
+@PageTitle("KML Demo")
+@DemoSource(
+    "https://github.com/FlowingCode/GoogleMapsAddon/blob/master/src/test/java/com/flowingcode/vaadin/addons/googlemaps/KMLLayerDemo.java")
+@SuppressWarnings("serial")
+public class KMLLayerDemo extends AbstractGoogleMapsDemo {
+
+  @Override
+  protected void createGoogleMapsDemo(String apiKey) {
+    GoogleMap gmaps = new GoogleMap(apiKey, null, null);
+    gmaps.setMapType(MapType.ROADMAP);
+    gmaps.setSizeFull();
+    gmaps.setKml("https://developers.google.com/maps/documentation/javascript/examples/kml/westcampus.kml");
+    add(gmaps);
+  }
+}
