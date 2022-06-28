1087520950977d3ccc186074889b9b4270392ffd
Paola De Bartolo <paola.debartolo@gmail.com>
2022-06-28 14:50:29 -0300

    chore: update license headers

--- src/test/java/com/flowingcode/vaadin/addons/googlemaps/AbstractGoogleMapsDemo.java
+++ src/test/java/com/flowingcode/vaadin/addons/googlemaps/AbstractGoogleMapsDemo.java
@@ -2,14 +2,14 @@
  * #%L
  * Google Maps Addon
  * %%
- * Copyright (C) 2020 - 2021 Flowing Code
+ * Copyright (C) 2020 - 2022 Flowing Code
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,6 +17,7 @@
  * limitations under the License.
  * #L%
  */
+
 package com.flowingcode.vaadin.addons.googlemaps;
 
 import com.vaadin.flow.component.dependency.StyleSheet;
